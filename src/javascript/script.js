1eef20904e7e514c33056a4f6ef1cd61f743ccd6
ygorp <ygor-nogueira@hotmail.com>
2024-11-14 15:32:19 -0300

    criando o menu mobile

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,3 +2,12 @@ $(window).on('scroll', function() {
   let header = $('#header');
   header.toggleClass('sticky', $(window).scrollTop() > 0);
 });
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
